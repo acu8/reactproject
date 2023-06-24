369777796048915f3f4a4f0bf295dc30d3afe789
acu8 <ewatanabe786@gmail.com>
2023-06-24 17:19:52 +0900

    react app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
+import 'bulma/css/bulma.css'
 import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
-function App() {
+function App(){
   return (
     <div>
-        <div>Personal Digital Assistant</div>
-        <ProfileCard title="Alexa" handle="@alexa99" />
-        <ProfileCard title="Contana" handle="@cortana88" />
-        <ProfileCard title="Siri" handle="@siri55" />
+        <section class="hero is-primary">
+  <div class="hero-body">
+    <p class="title">
+      Personal Digital Assistants
+    </p>
+  </div>
+</section>
+
+        <div className='container'>
+          <section className='section'>
+            <div className='columns'>
+              <div className='column is-4'>
+                <ProfileCard 
+                title="Alexa" 
+                handle="@alexa99" 
+                image={AlexaImage}
+                description="Alexa was created by Amazon and helps you buy things"
+                 />
+              </div>
+              <div className='column is-4'>
+                <ProfileCard 
+                title="Contana" 
+                handle="@cortana88" 
+                image={CortanaImage}
+                description="Contana was created by MS and helps you buy things"
+                
+                />
+              </div>
+              <div className='column is-4'>
+                <ProfileCard 
+                title="Siri" 
+                handle="@siri55" 
+                image={SiriImage}
+                description="Siri was created by MS and helps you buy things"
+          
+                />
+              </div>
+            </div>
+          </section>
+        </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import 'bulma/css/bulma.css'
 
-export default function ProfileCard(props) {
+export default function ProfileCard({ title, handle, image, description }) {
   return (
-    <>
-    <div>Title is {props.title}</div>
-    <div>Title is {props.handle}</div>
-    </>
+    <div className='card'>
+      <div className='card-image'>
+        <figure className='image is-1by1'>
+         <img src={image} alt='pda' />
+        </figure>
+      </div>
+    <div className='card-content'>
+      <div className='media-content'>
+        <p class="title is-4">Title is {title}</p>
+        <p class="subtitle is-6">Title is {handle}</p>
+      </div>
+      <div className='content'>{description}</div>
+    </div>
+  </div>
   )
 }
